a82b1bcbda906ab5ea74663e8ec868c73ebef368
Pau Marfany <p.marfany_96@hotmail.com>
2020-06-21 13:29:17 +0200

    Updates

---
+++ precache-manifest.c10c2081cc8b73a2e2e961c5827881b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eded1f137a411cde0927273ab7c0c02f",
+    "url": "/youtube-loop/index.html"
+  },
+  {
+    "revision": "22762b3976868941e306",
+    "url": "/youtube-loop/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "22762b3976868941e306",
+    "url": "/youtube-loop/static/js/2.d0f85b25.chunk.js"
+  },
+  {
+    "revision": "ac6b14caa396233aa9c8",
+    "url": "/youtube-loop/static/js/main.469e15fc.chunk.js"
+  },
+  {
+    "revision": "fee1f3e8a0c64fb1f382",
+    "url": "/youtube-loop/static/js/runtime~main.18798a0d.js"
+  }
+]);
