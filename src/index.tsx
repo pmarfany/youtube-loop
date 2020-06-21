20f8c47af7f9f935196f840dda65705ff204f99b
Pau Marfany <p.marfany_96@hotmail.com>
2020-06-21 19:46:58 +0200

    Add basename

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import {App} from './App';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/youtube-loop">
     <App/>
   </BrowserRouter>,
   document.getElementById('root')
